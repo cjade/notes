2bebe071d9979678978f8200df25a6b0d1c7b565
Jade <862226994@qq.com>
2018-11-07 21:31:05 +0800

    main

---
+++ golang/main.go
@@ -0,0 +1,15 @@
+package main
+
+import(
+    "net/http"
+    "fmt"
+)
+
+func main() {
+    http.HandleFunc("/zc",hello)
+    http.ListenAndServe(":8080",nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w,"Hello Docker Form Golang!")
+}
